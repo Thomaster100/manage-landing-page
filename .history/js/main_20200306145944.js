9ddf1e35d18e411e7f49f58031c3b1b0eda5cc29
Thomas088 <thomaskouadio92@gmail.com>
2020-03-06 15:57:02 +0100

    fix-background

---
+++ .history/js/main_20200306145944.js
@@ -0,0 +1,14 @@
+window.addEventListener("DOMContentLoaded", function () {
+
+const hamburger = document.querySelector(".container__hamburger");
+const navItems = document.querySelector(".block__items");
+const body = document.body;
+
+hamburger.addEventListener("click", function() {
+    hamburger.classList.toggle("--clicked");
+    navItems.classList.toggle("--visible");
+    body.classList.add("-nav--opened");
+})
+
+
+});
